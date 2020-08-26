ea48dbf6679118bea78c7aec00b4dc5f6a41bc7c
online135 <b97b01067@ntu.edu.tw>
2020-08-26 23:35:53 +0800

    Add files via upload

--- api.py
+++ api.py
@@ -53,7 +53,7 @@ def daily_aws_usage():
     lineItem_UsageAccountID = request.args.get('lineItem_UsageAccountID')
 
     StartEnd = """
-        select all product_ProductName, min(lineItem_UsageStartDate), max(lineItem_UsageEndDate), lineItem_UnblendedCost
+        select product_ProductName, min(lineItem_UsageStartDate), max(lineItem_UsageEndDate), lineItem_UsageAmount
         from aws_usage
         where lineItem_UsageAccountID = ?
         group by product_ProductName
@@ -62,9 +62,9 @@ def daily_aws_usage():
     query_date =db.engine.execute(StartEnd,lineItem_UsageAccountID).fetchall()
     print(query_date)
     
-    # query_date 為 list 裡面放多個tuple, 格式為 [(服務1, 最小日期, 最大日期),(服務2, 最小日期, 最大日期),(服務3, 最小日期, 最大日期)]
-    # query_date[0] 拿出 (服務1, 最小日期, 最大日期)
-    # query_date[0][0] 拿出 某服務; query_date[0][1] 拿出最小日期; query_date[0][2] 拿出最大日期; query_date[0][3] 拿出
+    # query_date 為 list 裡面放多個tuple, 格式為 [(服務1, 最小日期, 最大日期, 使用量),(服務2, 最小日期, 最大日期, 使用量),(服務3, 最小日期, 最大日期, 使用量)]
+    # query_date[0] 拿出 (服務1, 最小日期, 最大日期, 使用量)
+    # query_date[0][0] 拿出 服務1; query_date[0][1] 拿出最小日期; query_date[0][2] 拿出最大日期; query_date[0][3] 拿出使用量
     #datetime.strptime("2018-01-31", "%Y-%m-%d")
     print(query_date[0])
     print(query_date[0][0])
@@ -79,17 +79,17 @@ def daily_aws_usage():
 
     RangeDate = timedelta(days=1)
 
-    i = 0
+    i = 0  #天數
     while StartDate <= EndDate:
         i +=1
-        
+            
         StartDate = StartDate + RangeDate
     print(i)
 
     StartDate = datetime.strptime(query_date[1][1], "%Y-%m-%d")
     EndDate = datetime.strptime(query_date[1][2], "%Y-%m-%d")
     RangeDate = timedelta(days=1)
-    
+        
     for j in range(i):
         print(datetime.strptime(query_date[1][1], "%Y-%m-%d") + timedelta(days=j))
 
@@ -97,23 +97,40 @@ def daily_aws_usage():
 
     cur = conn.cursor()
 
-    cur.execute('DROP TABLE IF EXISTS temp')
+    tempProductName = query_date[1][0]
+    print(tempProductName)
+
+    cur.execute('DROP TABLE temp')
     
-    cur.execute('''
-        CREATE TABLE "temp"(
-        "TempTime" TEXT,
-        "Cost" TEXT)
-        ''')
-
-    for j in range(i):
-        TempTime = datetime.strptime(query_date[1][1], "%Y-%m-%d") + timedelta(days=j)
-
-        cur.execute('''INSERT INTO temp(TempTime,Cost)
-            VALUES (?,?)''',(TempTime,None))
-
-        conn.commit()
+    try:
         
+        sql_cmd = '''
+            CREATE TABLE "temp"(
+            "TempTime" TEXT,
+            "UsageAmount" TEXT,
+            "tempProductName" TEXT
+            )
+            '''
+        
+        cur.execute(sql_cmd)
+        
+        for j in range(i):
+            TempTime = datetime.strptime(query_date[1][1], "%Y-%m-%d") + timedelta(days=j)
+            UsageAmount = query_date[1][3] / i
+            cur.execute('''INSERT INTO temp(TempTime,UsageAmount,tempProductName) VALUES (?,?,?)''',(TempTime,UsageAmount,tempProductName))
 
+            conn.commit()
+    except:
+        UsageAmount = cur.execute('''SELECT UsageAmount FROM Temp''').fetchall()
+        print(UsageAmount)
+        for j in range(i):
+            TempTime = datetime.strptime(query_date[1][1], "%Y-%m-%d") + timedelta(days=j)
+            tempAmount = float(UsageAmount[1][0]) + query_date[1][3] / i
+#            UsageAmount[k][0] = list(UsageAmount[k][0])
+#            UsageAmount[k][0] = float(UsageAmount[k][0]) + query_date[i][3] / j
+            cur.execute('''INSERT INTO temp(TempTime,UsageAmount,tempProductName)
+                    VALUES (?,?,?)''',(TempTime,tempAmount,tempProductName))
+                
     return 'ok'
 
 
