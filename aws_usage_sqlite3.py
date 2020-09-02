fd5a77009b818d6d16a5f00ba4b2694d13cf6d9e
online135 <b97b01067@ntu.edu.tw>
2020-09-02 10:07:19 +0800

    Add files via upload

--- aws_usage_sqlite3.py
+++ aws_usage_sqlite3.py
@@ -28,10 +28,11 @@ CREATE TABLE "aws_usage"(
 
 
 fname=input('Enter the aws csv file name: ')
-if len(fname) < 1 : fname="output.csv"
+if len(fname) < 1 : fname="output_old.csv"
 
 with open(fname) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
     for row in csv_reader:
         bill_PayerAccountId=float(row[5])                    # The account ID of the paying account. For an organization in AWS Organizations, this is the account ID of the master account.
         lineItem_UnblendedCost=float(row[19])                # The UnblendedCost is the UnblendedRate multiplied by the UsageAmount.
@@ -43,13 +44,8 @@ with open(fname) as csv_file:
 
         lineItem_UsageAccountId=float(row[7])                # The ID of the account that used this line item. For organizations, this can be either the master account or a member account. You can use this field to track costs or usage by account.
         lineItem_UsageAmount=float(row[15])                  # The amount of usage that you incurred during the specified time period. For size-flexible reserved instances, use the reservation/TotalReservedUnits column instead.
-
-        StartDate = row[9].split("T", 1)
-        lineItem_UsageStartDate=StartDate[0]                 # The start date and time for the line item in UTC, inclusive. The format is YYYY-MM-DDTHH:mm:ssZ.
-
-        EndDate = row[10].split("T", 1)
-        lineItem_UsageEndDate=EndDate[0]                     # The end date and time for the corresponding line item in UTC, exclusive. The format is YYYY-MM-DDTHH:mm:ssZ.
-
+        lineItem_UsageStartDate = row[9].split("T", 1)[0]    # The start date and time for the line item in UTC, inclusive. The format is YYYY-MM-DDTHH:mm:ssZ.
+        lineItem_UsageEndDate = row[10].split("T", 1)[0]     # The end date and time for the corresponding line item in UTC, exclusive. The format is YYYY-MM-DDTHH:mm:ssZ.
         product_ProductName=row[21]                          # The full name of the AWS service. Use this column to filter AWS usage by AWS service. Sample values: AWS Backup, AWS Config, Amazon Registrar, Amazon Elastic File System, Amazon Elastic Compute Cloud
 
         cur.execute('''INSERT INTO aws_usage(bill_PayerAccountId,lineItem_UnblendedCost,lineItem_UnblendedRate,lineItem_UsageAccountId,lineItem_UsageAmount,lineItem_UsageStartDate,lineItem_UsageEndDate,product_ProductName)
